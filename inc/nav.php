2e38de04e0231c717fdfffbb28376fb8ce6610fe
Liconte1706 <anyelolathan@gmail.com>
2025-01-23 12:05:44 -0400

    Add files via upload

---
+++ inc/nav.php
@@ -0,0 +1,9 @@
+<nav class="nav">
+            <div class="logo">LOGO</div>
+            <ul class="menu">
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="historia.php">Historia </a></li>
+                <li><a href="contacto.php">Contacto</a></li>
+                <li><a href="login.php">Iniciar sesión</a></li>
+            </ul>
+</nav>
